496ac5de9a7852a7973fed782f9177b128737086
AlanC8 <askvizi@bk.ru>
2025-10-14 09:41:54 +0500

    v:0.2

--- practice/starter/models.py
+++ practice/starter/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    product_name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
