41e1fa65c090986e751bcb3d7e7b1b74fb2e4aa8
AlanC8 <askvizi@bk.ru>
2025-10-19 14:36:55 +0500

    midka

---
+++ users/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('Email', unique=True)
+    first_name = models.CharField('First name', max_length=150, blank=True)
+    last_name = models.CharField('Last name', max_length=150, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
+    
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def get_short_name(self):
+        return self.first_name
+    
